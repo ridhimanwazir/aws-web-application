2b03f8395568dc6ee79d720df3b27314377ee492
ridhimanwazir <ridhimansingh@gmail.com>
2024-01-23 11:05:41 +0530

    middleware

---
+++ middleware/app.py
@@ -0,0 +1,32 @@
+import boto3
+import uuid
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('TaskManager') 
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    response = table.scan()
+    tasks = response.get('Items', [])
+    task_list = [{'id': task['id'], 'title': task['title']} for task in tasks]
+    return jsonify({'tasks': task_list})
+
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    data = request.get_json()
+    new_task = {
+        'id': str(uuid.uuid4()),
+        'title': data['title']
+    }
+    table.put_item(Item=new_task)
+    return jsonify({'message': 'Task created successfully'}), 201
+
+@app.route('/tasks/<string:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    table.delete_item(Key={'id': task_id})
+    return jsonify({'message': 'Task deleted successfully'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
