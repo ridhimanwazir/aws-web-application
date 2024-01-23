4c7b4c9f59dff08026fb48374b385c5f00530bf9
ridhimanwazir <ridhimansingh@gmail.com>
2024-01-23 11:43:10 +0530

    middleware

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_ENDPOINT = 'http://44.203.151.27:5000';
 
 function App() {
   const [tasks, setTasks] = useState([]);
@@ -13,7 +12,7 @@ function App() {
 
   const fetchTasks = async () => {
     try {
-      const response = await axios.get('API_ENDPOINT/tasks');
+      const response = await axios.get('http://44.203.151.27:5000/tasks');
       setTasks(response.data.tasks);
     } catch (error) {
       console.error('Error fetching tasks:', error);
@@ -22,7 +21,7 @@ function App() {
 
   const createTask = async () => {
     try {
-      await axios.post('API_ENDPOINT/tasks', { title: newTaskTitle });
+      await axios.post('http://44.203.151.27:5000/tasks', { title: newTaskTitle });
       setNewTaskTitle('');
       fetchTasks();
     } catch (error) {
@@ -32,7 +31,7 @@ function App() {
 
   const deleteTask = async (taskId) => {
     try {
-      await axios.delete(`API_ENDPOINT/tasks/${taskId}`);
+      await axios.delete(`http://44.203.151.27:5000/tasks/${taskId}`);
       fetchTasks();
     } catch (error) {
       console.error('Error deleting task:', error);
