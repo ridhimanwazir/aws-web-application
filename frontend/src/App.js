b5439a447826ef9fb6dfe21190a4484e3bf42cba
ridhimanwazir <ridhimansingh@gmail.com>
2024-01-24 20:52:50 +0530

    backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ function App() {
 
   const fetchTasks = async () => {
     try {
-      const response = await axios.get('http://44.203.151.27:5000/tasks');
+      const response = await axios.get('http://middleware.us-east-1.elasticbeanstalk.com:5000/tasks');
       setTasks(response.data.tasks);
     } catch (error) {
       console.error('Error fetching tasks:', error);
@@ -21,7 +21,7 @@ function App() {
 
   const createTask = async () => {
     try {
-      await axios.post('http://44.203.151.27:5000/tasks', { title: newTaskTitle });
+      await axios.post('http://middleware.us-east-1.elasticbeanstalk.com:5000/tasks', { title: newTaskTitle });
       setNewTaskTitle('');
       fetchTasks();
     } catch (error) {
@@ -31,7 +31,7 @@ function App() {
 
   const deleteTask = async (taskId) => {
     try {
-      await axios.delete(`http://44.203.151.27:5000/tasks/${taskId}`);
+      await axios.delete(`http://middleware.us-east-1.elasticbeanstalk.com:5000/tasks/${taskId}`);
       fetchTasks();
     } catch (error) {
       console.error('Error deleting task:', error);
